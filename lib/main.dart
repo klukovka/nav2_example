7c9da24c9d2b28de30ea05e26e09303e4d0cd4dc
klukovka <yuliia.koba@proarea.com>
2023-09-19 18:40:08 +0300

    PreferensesService

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,5 @@
-void main() {}
+import 'package:hive_flutter/hive_flutter.dart';
+
+Future<void> main() async {
+  await Hive.initFlutter();
+}
---
+++ lib/services/preferences_service.dart
@@ -0,0 +1,29 @@
+import 'package:hive/hive.dart';
+import 'package:injectable/injectable.dart';
+
+@singleton
+@preResolve
+class PreferensesService {
+  static const _boxKey = '_boxKey';
+  static const _loggedInKey = '_loggedInKey';
+
+  final Box<bool> _box;
+
+  PreferensesService._(this._box);
+
+  @factoryMethod
+  Future<PreferensesService> getInstance() async {
+    final box = await Hive.openBox<bool>(_boxKey);
+    return PreferensesService._(box);
+  }
+
+  bool get isLoggedIn => _box.get(_boxKey) ?? false;
+
+  Future<void> login() async {
+    await _box.put(_loggedInKey, true);
+  }
+
+  Future<void> logout() async {
+    await _box.put(_loggedInKey, false);
+  }
+}
