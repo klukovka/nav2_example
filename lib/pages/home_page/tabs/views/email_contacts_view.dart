9190f973a7db7bda6fb01570b1488a4cdb57ac73
klukovka <yuliia.koba@proarea.com>
2023-09-20 17:14:51 +0300

    contact views

---
+++ lib/pages/home_page/tabs/views/email_contacts_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EmailContactsView extends StatelessWidget {
+  const EmailContactsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('SIM Contacts'),
+    );
+  }
+}
---
+++ lib/pages/home_page/tabs/views/sim_contacts_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SimContactsView extends StatelessWidget {
+  const SimContactsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('SIM Contacts'),
+    );
+  }
+}
